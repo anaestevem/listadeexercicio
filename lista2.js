a91b44175a53b7476a9c91f52922eda959f9a2b5
anaestevem <ana.estevem@escola.pr.gov.br>
2026-03-05 14:12:02 +0000

    lista

---
+++ lista2.js
@@ -0,0 +1,11 @@
+//criar um programa para verificar se oo numero 12 e divisivel por 2 e por 3
+//criar um programa para mostrar um desconto de 12% para prodrutos acimade 80R$.
+//criar um programa que mostre os numeros do 1 a 10 usando laço de repetição
+//criar um programa para mostrarapenas os numeros pares entre 1 e 20
+//criar um programa para mostrar apenas os numeros impares entre 1 a 20
+//criar um programa para mostrar a tabuada do 5
+//criar um programa para mostrar os numeros de 1 a 7 usando while.
+//criar um programa para mostrar a tabuada do 3 usando while
+//criar um programa para mostrar o fatorial de 5
+//criar um programa para saber se o numero 7 é primario
+//criar um programa para calcular a potencia de 2
